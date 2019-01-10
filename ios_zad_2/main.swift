5ccfd4f2902544f6297e4581f7895a8e0fc2e3a1
Karol Chojnacki <karol.chojnacki@icloud.copm>
2019-01-10 17:21:05 +0100

    Initial Commit

---
+++ ios_zad_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ios_zad_2
+//
+//  Created by Karol Chojnacki on 10/01/2019.
+//  Copyright © 2019 Karol Chojnacki. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
